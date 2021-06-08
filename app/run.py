95b05b79f32c4ccfa194a4952f822ce354918d37
Matt Burchett <matt@mattburchett.com>
2021-06-07 20:04:47 -0500

    Converting to fstring

--- app/run.py
+++ app/run.py
@@ -220,10 +220,10 @@ async def sync_to_cloudflare(cloudflare_token, cloudflare_zone, cloudflare_dns_s
     for vm in vms:
         if cloudflare_dns_subdomain:
             vm['name'] = vm['name'].replace(f".{cloudflare_dns_subdomain}.{cloudflare_zone}", '')
-            tasks.append(asyncio.create_task(cf.update_record(f"{vm['name'}.{cloudflare_dns_subdomain}.{cloudflare_zone}", vm['ip_address'])))
+            tasks.append(asyncio.create_task(cf.update_record(f"{vm['name']}.{cloudflare_dns_subdomain}.{cloudflare_zone}", vm['ip_address'])))
         else:
             vm['name'] = vm['name'].replace(f".{cloudflare_zone}", '')
-            tasks.append(asyncio.create_task(cf.update_record(f"{vm['name'].{cloudflare_zone}", vm['ip_address'])))
+            tasks.append(asyncio.create_task(cf.update_record(f"{vm['name']}.{cloudflare_zone}", vm['ip_address'])))
     await asyncio.gather(*tasks)
 
 async def pull_from_proxmox(proxmox_url, proxmox_nodes, proxmox_token_name, proxmox_token, network):
